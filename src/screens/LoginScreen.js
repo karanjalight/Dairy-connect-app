b2a96f53ea03b6185c5509bb297d29ddebd9bec3
karanjalight <karanjalight@gmail.com>
2024-02-01 13:10:51 +0300

    app structure setup

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,39 @@
+
+import React, { useState } from 'react'
+import { TouchableOpacity, StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+import TextInput from '../components/TextInput'
+import BackButton from '../components/BackButton'
+import { theme } from '../core/theme'
+import { emailValidator } from '../helpers/emailValidator'
+import { passwordValidator } from '../helpers/passwordValidator'
+
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Set Up</Header>
+      <Paragraph>
+        Welcome to Dairy Connect
+      </Paragraph>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate('Dashboard')}
+      >
+        Login
+      </Button>
+      <Button
+        mode="outlined"
+        onPress={() => navigation.navigate('RegisterScreen')}
+      >
+        Sign Up
+      </Button>
+    </Background>
+  )
+}
